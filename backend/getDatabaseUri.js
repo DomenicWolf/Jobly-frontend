d77ab0b2c2ebb399c2aa99b596d742ee6ed2987e
Domenic <domenicdrw@gmail.com>
2023-07-05 22:54:33 -0400

    changing things

--- backend/getDatabaseUri.js
+++
@@ -1,8 +0,0 @@
-const uri = () => {
-    return (process.env.NODE_ENV === "test")
-    ? `postgresql://dom:${"dom123321!"}@127.0.0.1:5432/jobly_test`
-    : process.env.DATABASE_URL || `postgresql://dom:${"dom123321!"}@127.0.0.1:5432/jobly`;
-}
-
-
-module.exports = uri
